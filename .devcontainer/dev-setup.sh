cf0f4a0983daff0282b9a4db6a537589d6fc4068
HRNPH <hrnph@protonmail.com>
2024-12-24 00:00:47 +0700

    first commit

---
+++ .devcontainer/dev-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# NOTE: Because of the issue in related to stopping the container, see: 
+# https://github.com/microsoft/vscode-remote-release/issues/3512#issuecomment-1267053890
+# The following installation commands have to be executed within the timeframe
+# set in the CMD command in the dockerfile.
+
+# Uncomment the sections you need and add your own commands
+# These are dependencies of tensorflow
+# Note that cuda-pytorch image used already installs many of these tools
+
+# # Update system
+apt-get update
+apt-get upgrade -y
+
+# Install Linux tools
+apt-get install software-properties-common
+
+# Install Python packages
+python3 -m pip install --upgrade pip
+pip3 install --user -r .devcontainer/requirements.txt
+
+# Clean up - for smaller image size
+sudo apt-get autoremove -y
+sudo apt-get clean
---
+++ README.md
@@ -0,0 +1,19 @@
+# Coder.com MR-Rehab Server - Research Template with GPU Support
+
+## Installed Software
+
+- Docker
+- Cuda Toolkit
+
+- python
+  - pyenv (with python 3.11)
+  - uv venv with following packages
+    - numpy
+    - pandas
+    - matplotlib
+    - seaborn
+    - scikit-learn
+    - torch
+    - jupyter
+    - ipython
+    - ipykernel
